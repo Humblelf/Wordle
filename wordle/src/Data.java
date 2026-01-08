8f36b1798a17f354abf20b491bb95b76b37575cd
Humblelf <792382634@qq.com>
2026-01-08 22:04:46 +0800

    Initial try

--- wordle/src/Data.java
+++ wordle/src/Data.java
@@ -2,11 +2,6 @@ public class Data
 {
     public Data()
     {
-        System.out.println("hello");
-        System.out.println("hello");
-        int a=0;
-        String d="hello";
-        double mm=1.0;
-        double ddd=3.0;
+
     }
 }
